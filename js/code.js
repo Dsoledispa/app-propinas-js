8b26897eafac7ca3fca53e95a0fb454473e4a3ea
Diego <diegojavi2026@gmail.com>
2021-10-08 19:51:09 +0200

    V2

--- js/code.js
+++ js/code.js
@@ -1,3 +1,12 @@
 function calculo() {
-    alert("hello");
+    //alert("hello");
+    let totalCuenta = document.getElementById('total').value;
+    //alert(totalCuenta);
+    //let servicio = document.getElementsByName('servicio')[0].value;
+    //alert(servicio);
+    //let personas = document.getElementsByClassName('personas')[0].value;
+    //alert(personas);
+    let servicio = document.getElementById('servicio').value;
+    let personas = document.getElementById('personas').value;
+    console.log('total: ' + totalCuenta + ' servicio: ' + servicio + ' personas: ' + personas);
 }
