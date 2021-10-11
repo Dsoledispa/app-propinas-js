f42395b4dbc06b979e0b0fea0377433ceacfac43
Diego <diegojavi2026@gmail.com>
2021-10-11 19:39:33 +0200

    V3

--- js/code.js
+++ js/code.js
@@ -8,5 +8,20 @@ function calculo() {
     //alert(personas);
     let servicio = document.getElementById('servicio').value;
     let personas = document.getElementById('personas').value;
-    console.log('total: ' + totalCuenta + ' servicio: ' + servicio + ' personas: ' + personas);
+    let resultado = (totalCuenta * servicio) / personas;
+    console.log('resultado: ' + resultado);
+    if (isNaN(resultado)) {
+        alert('Introduce datos validos');
+    } else if (personas == 0 || personas == null) {
+        alert('Introduce un valor para la cantidad de personas');
+    } else {
+        document.getElementById("res").innerHTML = "<p>total a pagar</p><br><h3>" + resultado + " por persona</h3>";
+    }
+    //console.log('total: ' + totalCuenta + ' servicio: ' + servicio + ' personas: ' + personas);
+}
+
+function limpiar() {
+    document.getElementById("resultado").innerHTML = "";
+    document.getElementById('personas').value = "";
+    document.getElementById('total').value = "";
 }
